86aa270a4a22e4fd83cf225241f6c9aea349e700
Riley Strong <strongriley@gmail.com>
2011-10-10 11:44:11 -0500

    CSSStyleDeclaration fix

--- d3.js
+++ d3.js
@@ -4,11 +4,15 @@
 try {
   document.createElement("div").style.setProperty("opacity", 0, "");
 } catch (error) {
-  var d3_style_prototype = CSSStyleDeclaration.prototype,
-      d3_style_setProperty = d3_style_prototype.setProperty;
-  d3_style_prototype.setProperty = function(name, value, priority) {
-    d3_style_setProperty.call(this, name, value + "", priority);
-  };
+  try {
+      var d3_style_prototype = CSSStyleDeclaration.prototype,
+          d3_style_setProperty = d3_style_prototype.setProperty;
+      d3_style_prototype.setProperty = function(name, value, priority) {
+        d3_style_setProperty.call(this, name, value + '', priority);
+      };
+  } catch (error) {
+    //alert('Please update your browser.');
+  }
 }
 d3 = {version: "2.2.0"}; // semver
 var d3_arraySubclass = [].__proto__?
