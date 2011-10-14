9fea98244fd26c2ca9203badd85fd377cbc4eeba
Riley Strong <strongriley@gmail.com>
2011-10-14 10:40:45 -0500

    complete.  Issue: Raphael doesn't support fill in the form of RGB as the CSS has it...

--- ex/dji.js
+++ ex/dji.js
@@ -57,16 +57,17 @@ Raphael.st.addClass = function(addClass, parentSelector) {
     }
 }
 
-Raphael.el.addClass = function(selector, parentSelector) {
+Raphael.el.addClass = function(addClass, parentSelector) {
     //easily add class
     if (Raphael.svg) {
-        var cssClass = this.node.getAttribute('class') !== null ? this.node.getAttribute('class') + ' ' + selector : selector;
+        var cssClass = this.node.getAttribute('class') !== null ? this.node.getAttribute('class') + ' ' + addClass : addClass;
         this.node.setAttribute('class', cssClass);
     }
     //must extract CSS requirements
     else {
         var sel = '.' + addClass;
         sel = parentSelector ? parentSelector + ' ' + sel : sel;
+
         var attributes = getCSSAttributes(sel);
         this.attr(attributes);
     }
@@ -76,12 +77,15 @@ function getCSSAttributes(selector) {
     var rules = getCSSRule(selector),
     attributes = {};
     if (!rules) return false;
-
+    alert('--'+selector);
+    alert(rules.style.cssText);
     rules = rules.style.cssText.split(';');
     for (var i = 0; i < rules.length; i++) {
         var rule = rules[i].split(':');
         if (rule[0] !== undefined && rule[1] !== undefined)
-            attributes[rule[0].replace(' ','')] = rule[1].replace(' ','');
+            var key = rule[0].replace(' ',''),
+                value = rule[1].replace(' ','');
+            attributes[key] = value;
     }
     return attributes;
 }
@@ -90,7 +94,7 @@ function getCSSAttributes(selector) {
 var papers = [],
 boxes = [],
 paper_i = -1,
-year_range = d3.range(1990, 1992);
+year_range = d3.range(1990, 1991);
 var test;
 vis = d3.select("#chart")
     .selectAll(type)
@@ -110,69 +114,77 @@ vis.select(function(data, i){
     return text.node;
 });
 
-//Days Rectangles
-vis.selectAll(Raphael.svg ? 'rect' : 'v:rect')
-    .data(calendar.dates, function(d, i) {
-        var rect;
-        //setup for each year: Raphael sets
-        if (i === 0) {
-            paper_i++;
-            boxes.push(papers[paper_i].set());
-            if(paper_i > 0) boxes[paper_i-1].addClass('day');
-        }
-        rect = papers[paper_i].rect(d.week * z + pw, d.day * z, z, z);
-        boxes[paper_i].push(rect); 
-        rect.node.__data__ = d;
-});//end data, last one
-boxes[paper_i].addClass('day');
-vis.selectAll(Raphael.svg ? 'rect' : 'v:rect')
-    .data(calendar.dates);
-paper_i = -1;
 
-
-//Months Path
-var months = [];
-vis.selectAll(Raphael.svg ? 'path' : 'v:path')
-    .data(calendar.months, function(d, i) {
-        //set paper & add month class to sets
-        if (i === 0) {
-            paper_i++;
-            months.push(papers[paper_i].set());
-            if(paper_i > 0) months[paper_i].addClass('month');
-        }
-
-        //draw month path
-        papers[paper_i].path(
-            "M" + (d.firstWeek + 1) * z + "," + d.firstDay * z
-            + "H" + d.firstWeek * z
-            + "V" + 7 * z
-            + "H" + d.lastWeek * z
-            + "V" + (d.lastDay + 1) * z
-            + "H" + (d.lastWeek + 1) * z
-            + "V" + 0
-            + "H" + (d.firstWeek + 1) * z
-            + "Z"
-        ).translate(pw,0);
-});//end data, last month set
-months[paper_i].addClass('month');
-paper_i = -1;
-var test;
 d3.csv("dji.csv", function(csv) {
-  var data = d3.nest()
-      .key(function(d) { return d.Date; })
-      .rollup(function(d) { return (d[0].Close - d[0].Open) / d[0].Open; })
-      .map(csv);
-  test = data;
+    var data = d3.nest()
+        .key(function(d) { return d.Date; })
+        .rollup(function(d) { return (d[0].Close - d[0].Open) / d[0].Open; })
+        .map(csv);
 
-  var color = d3.scale.quantize()
-      .domain([-.05, .05])
-      .range(d3.range(9));
+    var color = d3.scale.quantize()
+        .domain([-.05, .05])
+        .range(d3.range(9));
 
-  vis.selectAll("rect.day")
-      .attr("class", function(d) { return "day q" + color(data[d.Date]) + "-9"; })
-    .append("svg:title")
-      .text(function(d) { return d.Date + ": " + (data[d.Date] * 100).toFixed(1) + "%"; });
+    //Days Rectangles
+    vis.selectAll(Raphael.svg ? 'rect' : 'v:rect')
+        .data(calendar.dates)
+        .enter().select(function(d, i) {
+            var rect;
+            //setup for each year: Raphael sets
+            if (i === 0) {
+                paper_i++;
+                boxes.push(papers[paper_i].set());
+                if(paper_i > 0) boxes[paper_i-1].addClass('day');
+            }
+            rect = papers[paper_i]
+                        .rect(d.week * z + pw, d.day * z, z, z);
+
+            //rect.addClass("q" + color(data[d.Date]) + "-9", '.RdYlGn');
+            boxes[paper_i].push(rect); 
+
+            return rect.node;
+    });//end data, last one
+    boxes[paper_i].addClass('day');
+    paper_i = -1;
+
+    boxes[0][0].addClass("q3-9", '.RdYlGn');
+
+
+    //Months Path
+    var months = [];
+    vis.selectAll(Raphael.svg ? 'path' : 'v:path')
+        .data(calendar.months)
+        .enter().select(function(d, i) {
+            var path;
+            //set paper & add month class to sets
+            if (i === 0) {
+                paper_i++;
+                months.push(papers[paper_i].set());
+                if(paper_i > 0) months[paper_i].addClass('month');
+            }
+
+            //draw month path
+            path = papers[paper_i].path(
+                "M" + (d.firstWeek + 1) * z + "," + d.firstDay * z
+                + "H" + d.firstWeek * z
+                + "V" + 7 * z
+                + "H" + d.lastWeek * z
+                + "V" + (d.lastDay + 1) * z
+                + "H" + (d.lastWeek + 1) * z
+                + "V" + 0
+                + "H" + (d.firstWeek + 1) * z
+                + "Z"
+            ).translate(pw,0);
+            months[paper_i].push(path);
+
+            return path.node;
+    });//end data, last month set
+    months[paper_i].addClass('month');
+    paper_i = -1;
+    //--- END MONTH ---
 });
+
+$('body').attr('style', 'background: rgb(1,20,200)')
 
 //Final Safari forced-rendering
 for (var i = 0; i < papers.length; i++) {
